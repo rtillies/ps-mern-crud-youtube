7a7e9192c964dd8760e2550dab864caab571e4c3
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-12 12:38:19 -0400

    Modernize code

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -6,7 +6,8 @@ const getNotes = async (req, res) => {
   const notes = await Note.find();
 
   // respond with notes
-  res.json({ notes: notes });
+  // res.json({ notes: notes });
+  res.json({ notes });
 };
 
 // get note by id
@@ -18,23 +19,27 @@ const getNote = async (req, res) => {
   const note = await Note.findById(id);
 
   // respond with note
-  res.json({ note: note });
+  // res.json({ note: note });
+  res.json({ note });
 };
 
 // post new note
 const createNote = async (req, res) => {
   // get data from request body
-  const title = req.body.title;
-  const body = req.body.body;
+  // const title = req.body.title;
+  // const body = req.body.body;
+  const {title, body} = req.body
 
   // create note object
   const note = await Note.create({
-    title: title,
-    body: body,
+    // title: title,
+    // body: body,
+    title, body,
   });
 
   // respond with new note
-  res.json({ note: note });
+  // res.json({ note: note });
+  res.json({ note });
 };
 
 // update existing note
@@ -43,20 +48,23 @@ const updateNote = async (req, res) => {
   const id = req.params.id;
 
   // get data from request body
-  const title = req.body.title;
-  const body = req.body.body;
+  // const title = req.body.title;
+  // const body = req.body.body;
+  const {title, body} = req.body
   
   // find and update note using id
   await Note.findByIdAndUpdate(id, {
-    title: title,
-    body: body,
+    // title: title,
+    // body: body,
+    title, body,
   });
 
   // Get updated note
   const note = await Note.findById(id)
 
   // respond with note
-  res.json({ note: note });
+  // res.json({ note: note });
+  res.json({ note });
 }
 
 const deleteNote = async (req, res) => {
@@ -67,13 +75,21 @@ const deleteNote = async (req, res) => {
   const note = await Note.findByIdAndDelete(id)
 
   // respond with deleted note
-  res.json({note: note})
+  // res.json({ note: note });
+  res.json({ note });
 };
 
 module.exports = {
-  getNote: getNote,
-  getNotes: getNotes,
-  createNote: createNote,
-  updateNote: updateNote,
-  deleteNote: deleteNote,
+  getNote,
+  getNotes,
+  createNote,
+  updateNote,
+  deleteNote,
 }
+// module.exports = {
+//   getNote: getNote,
+//   getNotes: getNotes,
+//   createNote: createNote,
+//   updateNote: updateNote,
+//   deleteNote: deleteNote,
+// }
