bd85d2dfa69408d3ee10078e591ea549663a24fa
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-12 12:32:49 -0400

    Update routes

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ if (process.env.NODE_ENV != "production") {
 // import dependencies
 const express = require("express");
 const connectDB = require("./config/connectDB");
-const Note = require("./models/note");
+const notesController = require("./controllers/notesController")
+// const Note = require("./models/note");
 
 // create and configure express app
 const app = express();
@@ -22,75 +23,11 @@ app.get("/", (req, res) => {
   res.json({ hello: "world" });
 });
 
-// Get all notes
-app.get("/notes", async (req, res) => {
-  // find notes
-  const notes = await Note.find();
-
-  // respond with notes
-  res.json({ notes: notes });
-});
-
-// get note by id
-app.get("/notes/:id", async (req, res) => {
-  // get id from URL
-  const id = req.params.id;
-
-  // find note using id
-  const note = await Note.findById(id);
-
-  // respond with note
-  res.json({ note: note });
-});
-
-// post new note
-app.post("/notes", async (req, res) => {
-  // get data from request body
-  const title = req.body.title;
-  const body = req.body.body;
-
-  // create note object
-  const note = await Note.create({
-    title: title,
-    body: body,
-  });
-
-  // respond with new note
-  res.json({ note: note });
-});
-
-// update existing note
-app.put("/notes/:id", async (req, res) => {
-  // get id from URL
-  const id = req.params.id;
-
-  // get data from request body
-  const title = req.body.title;
-  const body = req.body.body;
-  
-  // find and update note using id
-  await Note.findByIdAndUpdate(id, {
-    title: title,
-    body: body,
-  });
-
-  // Get updated note
-  const note = await Note.findById(id)
-
-  // respond with note
-  res.json({ note: note });
-})
-
-app.delete('/notes/:id', async (req, res) => {
-  // get id from URL
-  const id = req.params.id;
-
-  // delete record
-  const note = await Note.findByIdAndDelete(id)
-
-  // respond with deleted note
-  res.json({note: note})
-})
+app.get("/notes", notesController.getNotes);
+app.get("/notes/:id", notesController.getNote);
+app.get("/notes/", notesController.createNote);
+app.get("/notes/:id", notesController.updateNote);
+app.get("/notes/:id", notesController.deleteNote);
 
 // Start server
 app.listen(process.env.PORT || 3000);
