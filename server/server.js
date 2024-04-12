ff6a078826e636931a81c1342242cc8c91793ac0
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-12 11:45:28 -0400

    Create post route for new note

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ if (process.env.NODE_ENV != 'production') {
 // import dependencies
 const express = require('express')
 const connectDB = require('./config/connectDB')
+const Note = require('./models/note')
 
-// create express app
+// create and configure express app
 const app = express();
+app.use(express.json())
 
 // Connect to database
 connectDB();
@@ -17,6 +19,20 @@ connectDB();
 app.get('/', (req, res) => {
   res.json({hello: "world"})
 });
+
+app.post('/notes', async (req, res) => {
+  // get the sent data from request body
+  const title = req.body.title;
+  const body = req.body.body;
+  
+  // create note object
+  const note = await Note.create({
+    title: title,
+    body: body,
+  })
+  // respond with new note
+  res.json({note: note})
+})
 
 // Start server
 app.listen(process.env.PORT || 3000);
