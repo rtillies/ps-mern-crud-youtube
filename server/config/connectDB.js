f9da99804f343f0d88d1c6568f97d0871b40fe99
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-12 11:36:46 -0400

    Connect to actual database

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,5 +1,17 @@
-function connectDB() {
-  console.log("Hello World");
+// load env variables
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config()
+}
+
+const mongoose = require('mongoose')
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.DB_URL)
+    console.log("Connected to database!");
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 module.exports = connectDB;
