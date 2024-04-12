cc55a6f9e082d25a257a4e35427623bc001dd3d7
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-12 12:07:13 -0400

    Get note by id

--- server/server.js
+++ server/server.js
@@ -1,38 +1,62 @@
 // load env variables
-if (process.env.NODE_ENV != 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 
 // import dependencies
-const express = require('express')
-const connectDB = require('./config/connectDB')
-const Note = require('./models/note')
+const express = require("express");
+const connectDB = require("./config/connectDB");
+const Note = require("./models/note");
 
 // create and configure express app
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 // Connect to database
 connectDB();
 
 // Routing
-app.get('/', (req, res) => {
-  res.json({hello: "world"})
+
+// Base route
+app.get("/", (req, res) => {
+  res.json({ hello: "world" });
 });
 
-app.post('/notes', async (req, res) => {
+// Get all notes
+app.get("/notes", async (req, res) => {
+  // find notes
+  const notes = await Note.find();
+
+  // respond with notes
+  res.json({ notes: notes });
+});
+
+// get note by id
+app.get("/notes/:id", async (req, res) => {
+  // get id from URL
+  const id = req.params.id;
+
+  // find note using id
+  const note = await Note.findById(id);
+
+  // respond with note
+  res.json({ note: note });
+});
+
+// post new note
+app.post("/notes", async (req, res) => {
   // get the sent data from request body
   const title = req.body.title;
   const body = req.body.body;
-  
+
   // create note object
   const note = await Note.create({
     title: title,
     body: body,
-  })
+  });
   // respond with new note
-  res.json({note: note})
-})
+  res.json({ note: note });
+});
 
 // Start server
 app.listen(process.env.PORT || 3000);
