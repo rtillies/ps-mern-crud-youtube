7e9bc068a674707a566b6bde4653c57e8f4758a5
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-13 12:38:36 -0400

    Create individual Note component

--- client/src/components/Notes/Notes.jsx
+++ client/src/components/Notes/Notes.jsx
@@ -1,4 +1,5 @@
 import notesStore from "../../stores/notesStore";
+import Note from "../Note/Note";
 
 export default function Notes() {
   const store = notesStore();
@@ -9,15 +10,16 @@ export default function Notes() {
       {store.notes &&
         store.notes.map((note) => {
           return (
-            <div key={note._id}>
-              <h3>{note.title}</h3>
-              <button onClick={() => store.toggleUpdate(note)}>
-                Update note
-              </button>
-              <button onClick={() => store.deleteNote(note._id)}>
-                Delete note
-              </button>
-            </div>
+            <Note note={note} key={note._id} />
+            // <div key={note._id}>
+            //   <h3>{note.title}</h3>
+            //   <button onClick={() => store.toggleUpdate(note)}>
+            //     Update note
+            //   </button>
+            //   <button onClick={() => store.deleteNote(note._id)}>
+            //     Delete note
+            //   </button>
+            // </div>
           );
         })}
     </div>
