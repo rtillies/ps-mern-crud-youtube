ba06ac99ce0af14a24dd96f94ab6e76f12c4a19d
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-13 14:39:44 -0400

    Wrap functions in try-catch

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 async function signup(req, res) {
@@ -15,51 +15,67 @@ async function signup(req, res) {
 
     // response
     res.sendStatus(200); // OK
-  } catch (e) { // probably duplicate email
+  } catch (e) {
+    // probably duplicate email
     console.log(e.message);
     res.sendStatus(400); // Bad Request
   }
 }
 
 async function login(req, res) {
-  // get email and password from req body
-  const { email, password } = req.body;
+  try {
+    // get email and password from req body
+    const { email, password } = req.body;
 
-  // find user with email
-  // if no user, return error
-  const user = await User.findOne({email})
-  if (!user) return res.sendStatus(401) // unauthorized
-  
-  // compare passwords
-  // if passwords do not match, return error
-  const passwordMatch = bcrypt.compareSync(password, user.password);
-  if (!passwordMatch) return res.sendStatus(401) // unauthorized
-  
-  // create jwt token
-  // const exp = Date.now() + 1000 * 5 // 5 seconds
-  const exp = Date.now() + 1000 * 60 * 60 * 24 * 30; // 30 days
-  const token = jwt.sign({ sub: user._id, exp }, process.env.SECRET);
+    // find user with email
+    // if no user, return error
+    const user = await User.findOne({ email });
+    if (!user) return res.sendStatus(401); // unauthorized
 
-  // set cookie
-  res.cookie("Authorization", token, {
-    expires: new Date(exp),
-    httpOnly: true,
-    sameSite: 'lax',
-    secure: process.env.NODE_ENV === 'production',
-  })
-  // send token
-  // res.json({token})
-  res.sendStatus(200); // ok
+    // compare passwords
+    // if passwords do not match, return error
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+    if (!passwordMatch) return res.sendStatus(401); // unauthorized
+
+    // create jwt token
+    // const exp = Date.now() + 1000 * 5 // 5 seconds
+    const exp = Date.now() + 1000 * 60 * 60 * 24 * 30; // 30 days
+    const token = jwt.sign({ sub: user._id, exp }, process.env.SECRET);
+
+    // set cookie
+    res.cookie("Authorization", token, {
+      expires: new Date(exp),
+      httpOnly: true,
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    });
+    // send token
+    // res.json({token})
+    res.sendStatus(200); // ok
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(400);
+  }
 }
 
 function logout(req, res) {
-  res.clearCookie("Authorization")
-  res.sendStatus(200);
+  try {
+    res.clearCookie("Authorization");
+    res.sendStatus(200);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(400);
+  }
 }
 
 function checkAuth(req, res) {
-  console.log(req.user);
-  res.sendStatus(200);
+  try {
+    console.log(req.user);
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(e);
+    res.sendStatus(400);
+  }
 }
 
 module.exports = {
