9d637d91d2b9f0a8b3da97ccac42ad518348d83a
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-13 09:36:33 -0400

    Create form for new notes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,49 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
   const [notes, setNotes] = useState(null);
   useEffect(() => {
-    getNotes()
-  }, [])
+    getNotes();
+  }, []);
 
   const getNotes = async () => {
-    const URL = 'http://localhost:3000/notes'
+    const URL = "http://localhost:3000/notes";
 
     // get notes
-    const res = await axios.get(URL)
+    const res = await axios.get(URL);
 
     // set on state
     console.log(res);
-    setNotes(res.data.notes)
-  }
+    setNotes(res.data.notes);
+  };
 
   return (
     <>
       <div className="App">
-        <h2>Notes</h2>
-        {notes && notes.map(note => {
-          return <div key={note._id}>
-            <h3>{note.title}</h3>
-          </div>
-        })}
+        <div className="notes">
+          <h2>Notes</h2>
+          {notes && notes.map((note) => {
+            return (
+              <div key={note._id}>
+                <h3>{note.title}</h3>
+              </div>
+              );
+            })}
+        </div>
+
+        <div className="create-note">
+          <h2>Create Note</h2>
+          <form action="" method="post">
+            <input type="text" name="title" />
+            <textarea name="body" cols="30" rows="10"></textarea>
+            <button type="submit">Create Note</button>
+          </form>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
