093299aa1de64dee1347f3dfdb55ca7f04df5957
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-13 13:25:44 -0400

    User controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,17 @@
+const User = require('../models/user')
+
+function signup(req, res) {
+  
+}
+
+function login(req, res) {
+
+}
+
+function logout(req, res) {
+
+}
+
+module.exports = {
+  signup, login, logout
+}
