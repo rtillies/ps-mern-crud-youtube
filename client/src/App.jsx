042f6099149ed7ec3b97cb6b377744d44c6eb703
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-13 09:26:50 -0400

    Render notes on website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,17 +10,24 @@ function App() {
 
   const getNotes = async () => {
     const URL = 'http://localhost:3000/notes'
+
     // get notes
-    const res = axios.get(URL)
+    const res = await axios.get(URL)
 
     // set on state
     console.log(res);
+    setNotes(res.data.notes)
   }
 
   return (
     <>
       <div className="App">
-        <h1>Hello World</h1>
+        <h2>Notes</h2>
+        {notes && notes.map(note => {
+          return <div key={note._id}>
+            <h3>{note.title}</h3>
+          </div>
+        })}
       </div>
     </>
   )
