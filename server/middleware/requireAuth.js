a934cf2cec38eee99169c46f408311650df293c9
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-13 14:20:52 -0400

    read and decode token, attach user

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -1,8 +1,27 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-function requireAuth(req, res, next) {
-  console.log("in middleware");
-  next()
+async function requireAuth(req, res, next) {
+  try {
+    // read token
+    const token = req.cookies.Authorization;
+
+    // decode token
+    const decoded = jwt.verify(token, process.env.SECRET);
+
+    // find user using decoded sub
+    const user = await User.findById(decoded.sub);
+    if (!user) return res.sendStatus(401);
+
+    // attach user to request
+    req.user = user;
+
+    // continue
+    console.log("in middleware");
+    next();
+  } catch (error) {
+    return res.sendStatus(401);
+  }
 }
 
 module.exports = requireAuth;
