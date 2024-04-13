e4c1b2cb0540bd23804f60ac279f4cd282deb69d
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-13 13:50:28 -0400

    get login from user, check email and password

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -20,7 +20,25 @@ async function signup(req, res) {
   }
 }
 
-function login(req, res) {}
+async function login(req, res) {
+  // get email and password from req body
+  const { email, password } = req.body;
+
+  // find user with email
+  // if no user, return error
+  const user = await User.findOne({email})
+  if (!user) return res.sendStatus(401) // unauthorized
+  
+  // compare passwords
+  // if passwords do not match, return error
+  const passwordMatch = bcrypt.compareSync(password, user.password);
+  if (!passwordMatch) return res.sendStatus(401) // unauthorized
+  
+  // create jwt token
+  
+
+  // send token
+}
 
 function logout(req, res) {}
 
