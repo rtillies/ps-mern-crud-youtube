fe7f46ae991803bc323f96c1f827b3be268f32be
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-13 12:23:32 -0400

    updateNote moved to store

--- client/src/stores/notesStore.jsx
+++ client/src/stores/notesStore.jsx
@@ -145,25 +145,3 @@ const notesStore = create((set) => ({
 }))
 
 export default notesStore;
-
-// export default function Notes() {
-//   return (
-//     <div className="notes">
-//     <h2>Notes</h2>
-//     {notes &&
-//       notes.map((note) => {
-//         return (
-//           <div key={note._id}>
-//             <h3>{note.title}</h3>
-//             <button onClick={() => toggleUpdate(note)}>
-//               Update note
-//             </button>
-//             <button onClick={() => deleteNote(note._id)}>
-//               Delete note
-//             </button>
-//           </div>
-//         );
-//     })}
-//   </div>
-// )
-// }
