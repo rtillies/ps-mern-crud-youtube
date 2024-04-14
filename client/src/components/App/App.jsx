a88d55cf1406b2223195c931cf05afbaeb8dfede
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-14 13:18:25 -0400

    Links to home and Login pages

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -6,7 +6,7 @@ import UpdateForm from "../UpdateForm/UpdateForm";
 import CreateForm from "../CreateForm/CreateForm";
 import "./App.css";
 import NotesPage from "../../pages/NotesPage";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import LoginPage from "../../pages/LoginPage";
 
 function App() {
@@ -121,6 +121,11 @@ function App() {
     <>
       <div className="App">
         <BrowserRouter>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/login'>Login</Link></li>
+          </ul>
+
           <Routes>
             <Route index element={<NotesPage />} />
             <Route path='/login' element={<LoginPage />} />
