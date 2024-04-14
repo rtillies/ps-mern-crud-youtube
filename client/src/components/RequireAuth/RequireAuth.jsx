b5ddad140f8efe8b8973b82f91436d97a8f8fd37
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-14 13:57:00 -0400

    create RequireAuth component

--- client/src/components/RequireAuth/RequireAuth.jsx
+++ client/src/components/RequireAuth/RequireAuth.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import authStore from '../../stores/authStore'
 
-export default function RequireAuth() {
+export default function RequireAuth(props) {
+  const store = authStore()
+
+  if (!store.loggedIn) {
+    return <div>Please Login!</div>
+  }
   return (
-    <div>RequireAuth</div>
+    <div>{props.children}</div>
   )
 }
