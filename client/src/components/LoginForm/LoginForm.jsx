86628c2edb965eeb4f8053c4d089a3aaaa4c588a
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-14 13:21:16 -0400

    Create Login Form component

---
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function LoginForm() {
+  return (
+    <form>
+      <input type="email" name="email" />
+      <input type="password" name="password" />
+      <button type="submit">Login</button>
+    </form>
+  )
+}
