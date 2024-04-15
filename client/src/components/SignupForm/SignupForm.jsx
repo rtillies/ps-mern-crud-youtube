b3f69a1a966c2df252697bcd93b5a0b8582e1f98
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 09:30:07 -0400

    Create shell for Signup

---
+++ client/src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {useNavigate} from 'react-router-dom';
+import authStore from "../../stores/authStore";
+
+export default function SignupForm() {
+  // const store = authStore();
+  // const navigate = useNavigate();
+
+  // const handleSignup = async (e) => {
+  //   e.preventDefault();
+  //   await store.signup();
+
+  //   // Navigate
+  //   navigate('/login')
+  // }
+
+  return (
+    <form onSubmit={handleSignup}>
+      <input
+        type="email"
+        name="email"
+        placeholder="email"
+        value={store.signupForm.email}
+        onChange={store.updateSignupForm}
+      />
+      <br />
+      <input
+        type="password"
+        name="password"
+        placeholder="password"
+        value={store.signupForm.password}
+        onChange={store.updateSignupForm}
+      />
+      <br />
+      <button type="submit">Signup</button><br />
+    </form>
+  );
+}
---
+++ client/src/pages/SignupPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignupForm from '../components/SignupForm/SignupForm'
+
+export default function SignupPage() {
+  return (
+    <>
+      <h2>Signup</h2>
+      <SignupForm />
+    </>
+  )
+}
