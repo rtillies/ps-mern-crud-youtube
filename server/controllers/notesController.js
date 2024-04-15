848ae70958d84cd17bd8b5cb9b996d0cc151a8ed
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-15 10:56:27 -0400

    add try-catch blocks to all functions

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -1,84 +1,103 @@
-const Note = require('../models/note')
+const Note = require("../models/note");
 
 // Get all notes
 const getNotes = async (req, res) => {
-  // find notes
-  const notes = await Note.find({user: req.user._id});
-
-  // respond with notes
-  // res.json({ notes: notes });
-  res.json({ notes });
+  try {
+    // find and return notes
+    const notes = await Note.find({ user: req.user._id });
+    res.json({ notes });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
 };
 
 // get note by id
 const getNote = async (req, res) => {
-  // get id from URL
-  const id = req.params.id;
+  try {
+    // get id from URL
+    const id = req.params.id;
 
-  // find note using id and user
-  // const note = await Note.findById(id);
-  const note = await Note.findOne({_id: id, user: req.user._id});
+    // find note using id and user
+    // const note = await Note.findById(id);
+    const note = await Note.findOne({ _id: id, user: req.user._id });
 
-  // respond with note
-  // res.json({ note: note });
-  res.json({ note });
+    // respond with note
+    // res.json({ note: note });
+    res.json({ note });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
 };
 
 // post new note
 const createNote = async (req, res) => {
-  // get data from request body
-  // const title = req.body.title;
-  // const body = req.body.body;
-  const {title, body} = req.body
+  try {
+    // get data from request body
+    const { title, body } = req.body;
 
-  // create note object
-  const note = await Note.create({
-    // title: title,
-    // body: body,
-    title, body,
-    user: req.user._id
-  });
+    // create note object
+    const note = await Note.create({
+      title,
+      body,
+      user: req.user._id,
+    });
 
-  // respond with new note
-  // res.json({ note: note });
-  res.json({ note });
+    // respond with new note
+    res.json({ note });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
 };
 
 // update existing note
 const updateNote = async (req, res) => {
-  // get id from URL
-  const id = req.params.id;
+  try {
+    // get id from URL
+    const id = req.params.id;
 
-  // get data from request body
-  // const title = req.body.title;
-  // const body = req.body.body;
-  const {title, body} = req.body
-  
-  // find and update note using id
-  // await Note.findByIdAndUpdate(id, {
-  await Note.findOneAndUpdate({_id: id, user: req.user._id}, {
-    title, body,
-  });
+    // get data from request body
+    const { title, body } = req.body;
 
-  // Get updated note
-  const note = await Note.findById(id)
+    // find and update note using id
+    // await Note.findByIdAndUpdate(id, {
+    await Note.findOneAndUpdate(
+      { _id: id, user: req.user._id },
+      {
+        title,
+        body,
+      }
+    );
 
-  // respond with note
-  // res.json({ note: note });
-  res.json({ note });
-}
+    // Get updated note
+    const note = await Note.findById(id);
+
+    // respond with note
+    res.json({ note });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
 
 const deleteNote = async (req, res) => {
-  // get id from URL
-  const id = req.params.id;
+  try {
+    // get id from URL
+    const id = req.params.id;
 
-  // delete record
-  // const note = await Note.findByIdAndDelete(id)
-  const note = await Note.findOneAndDelete({_id: id, user: req.user._id})
+    // delete record
+    // const note = await Note.findByIdAndDelete(id)
+    const note = await Note.findOneAndDelete({ _id: id, user: req.user._id });
 
-  // respond with deleted note
-  // res.json({ note: note });
-  res.json({ note });
+    // respond with deleted note
+    // res.json({ note: note });
+    res.json({ note });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
 };
 
 module.exports = {
@@ -87,11 +106,4 @@ module.exports = {
   createNote,
   updateNote,
   deleteNote,
-}
-// module.exports = {
-//   getNote: getNote,
-//   getNotes: getNotes,
-//   createNote: createNote,
-//   updateNote: updateNote,
-//   deleteNote: deleteNote,
-// }
+};
